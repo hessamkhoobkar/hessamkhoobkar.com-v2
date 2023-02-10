9a0edd1be1cfbd00c5f5d7c21edafb392ff3aa9a
hessam khoobkar <hessam.khoobkar@coinhaven.com>
2023-02-11 00:03:36 +0330

    sidenav init

---
+++ src/components/layout/SideNav.svelte
@@ -0,0 +1,11 @@
+<div class="bg-gray-1000 w-80 h-screen">
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sit repellat laudantium
+		quaerat rem dolores in reiciendis quos delectus accusantium doloribus voluptatum, quasi veniam
+		nobis quod reprehenderit nesciunt eius dolorem.
+	</p>
+
+	<!-- Head Logo -->
+	<!-- Nav links -->
+	<!-- Footer Card -->
+</div>
