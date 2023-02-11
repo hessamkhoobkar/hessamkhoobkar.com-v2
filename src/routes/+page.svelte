06d4875dcd696749acac10188a641638efa9665b
hessam khoobkar <hessam.khoobkar@coinhaven.com>
2023-02-11 18:39:37 +0330

    remove extra styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,2 @@
 <h1 class="text-3xl font-bold text-cyan-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<style lang="postcss">
-	:global(html) {
-		color: theme(colors.gray.100);
-		background-color: theme(colors.gray.900);
-	}
-</style>
