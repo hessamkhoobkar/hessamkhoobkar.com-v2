4ec7d7f157dc3cc874f2a473adfa2c2afe968c49
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-07 18:22:13 +0330

    init pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1 class="text-3xl font-bold text-cyan-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import InfoCard from '@/components/dashboard/InfoCard.svelte';
+	import GameChart from '@/components/dashboard/GameChart.svelte';
+	import TypeChart from '@/components/dashboard/TypeChart.svelte';
+	import References from '@/components/dashboard/References.svelte';
+	import CurrentBook from '@/components/dashboard/CurrentBook.svelte';
+
+	const summaryData = [
+		{ title: 'Total Projects', value: '12', subNote: 'Projects finished and delivered' },
+		{ title: 'Daily tea intake', value: '5', subNote: 'Cup of tea per day' },
+		{ title: 'Life under control', value: '64%', subNote: '23% improvement from last year ' }
+	];
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-6">
+	{#each summaryData as { title, value, subNote }}
+		<div class="col-span-4 p-4 rounded-2xl bg-gray-850 shadow-lg shadow-gray-900">
+			<p class="text-base mb-6">
+				{title}
+			</p>
+			<p class="text-6xl font-bold text-sky-500 mb-2">
+				{value}
+			</p>
+			<p class="text-xs text-sky-500 text-opacity-60">
+				{subNote}
+			</p>
+		</div>
+	{/each}
+	<InfoCard />
+	<CurrentBook />
+	<TypeChart />
+	<References />
+	<GameChart />
+</div>
+
+<!-- Leet Code and Github -->
+<!-- Skills, programming languages, frameworks, tools, and languages  -->
+<!-- Work history -->
---
+++ src/routes/themes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div class="flex justify-between items-center gap-4">
+	<h3>Hello World</h3>
+</div>
