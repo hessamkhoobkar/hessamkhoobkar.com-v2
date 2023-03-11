42c536f0386e2295ac57449c358cf578f3737463
hessam_khoobkar <41147418+hessamkhoobkar@users.noreply.github.com>
2023-03-11 21:34:50 +0330

    add <svelte:head> meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,18 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<meta name="title" content="Hessam Khoobkar | Front End Developer and UI Designer Portfolio" />
+	<meta
+		name="description"
+		content="Hessam Khoobkar's portfolio website showcasing his front-end development and UI design skills. Get in touch for web development collaborations."
+	/>
+	<meta
+		name="keywords"
+		content="front-end developer, front end developer, UI designer, web development, HTML, CSS, JavaScript, responsive design, user experience, user interface, portfolio"
+	/>
+</svelte:head>
+
 <slot />
 
 <style lang="postcss">
