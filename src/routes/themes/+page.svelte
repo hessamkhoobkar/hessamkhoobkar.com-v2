8698d55e0b0ee1c05f97f7c63ed2c989f205a375
hessam_khoobkar <amirhessam.dev@gmail.com>
2023-06-20 16:53:17 +0330

    start version 2 of my website

--- src/routes/themes/+page.svelte
+++ src/routes/themes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	const PlaceholderTitle = "Hello world, I'm theme";
 </script>
 
 <div class="flex justify-between items-center gap-4">
-	<h3>Hello World</h3>
+	<h3>{PlaceholderTitle}</h3>
 </div>
