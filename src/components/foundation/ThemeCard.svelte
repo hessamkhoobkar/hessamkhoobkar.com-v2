86c66f85e06ed1449de07d16d305ef02692ad049
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-01-10 14:39:05 +0330

    Update ThemeCard link behavior

--- src/components/foundation/ThemeCard.svelte
+++ src/components/foundation/ThemeCard.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="w-full rounded-2xl bg-gray-800 border border-gray-600">
-	<a class="w-full h-full flex flex-col p-4" {href}>
+	<a class="w-full h-full flex flex-col p-4 {href === '#' && 'cursor-default'}" {href}>
 		<div class="w-full aspect-w-4 aspect-h-3 rounded-xl overflow-hidden">
 			<img class="object-cover" src={thumbnail} alt={thumbnailAlt} />
 		</div>
