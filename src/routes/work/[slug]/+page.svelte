74eb87308a20cbce4393930a291bb67aa54cb3ea
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-01-21 18:13:54 +0330

    add Details section to work page

--- src/routes/work/[slug]/+page.svelte
+++ src/routes/work/[slug]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { formatDate } from '$lib/utils';
 	import ExternalLink from '@/assets/icons/ExternalLink.svelte';
-	import PageHero from '@/components/layout/PageHero.svelte';
+	import WorkChip from '@/components/work/WorkChip.svelte';
 
 	export let data;
 </script>
@@ -23,37 +23,71 @@
 			src={data.meta.coverImage}
 			alt={data.meta.coverImageAlt}
 		/>
-		<p class="mt-6 font-medium text-gray-400 text-sm opacity-75">
-			Published at {formatDate(data.meta.date)} . Author: Hessam Khoobkar (me)
-		</p>
-		<div class="bg-gradient-to-br from-primary-500 to-tertiary-500 text-transparent bg-clip-text">
+		<div class="bg-gradient-to-br from-gray-500 to-white text-transparent bg-clip-text mt-4">
 			<h1 class="text-6xl font-bold">{data.meta.title}</h1>
 		</div>
 	</div>
 
-	<div class="flex gap-4 mb-12 font-bold">
-		<a
-			target="_blank"
-			href={data.meta.repository}
-			class="w-full h-full flex justify-between p-4 py-6 grow rounded-2xl bg-gray-800 border border-gray-600 text-gray-300 hover:border-primary-500 hover:text-primary-500 transition-all duration-200 ease-in-out"
-		>
-			<span> GitHub repository </span>
-			<ExternalLink class="w-6 h-6" />
-		</a>
+	<!-- Details -->
 
-		<a
-			target="_blank"
-			href={data.meta.live}
-			class="w-full h-full flex justify-between p-4 py-6 grow rounded-2xl bg-gray-800 border border-gray-600 text-gray-300 hover:border-primary-500 hover:text-primary-500 transition-all duration-200 ease-in-out"
-		>
-			<span> live demo </span>
-			<ExternalLink class="w-6 h-6" />
-		</a>
+	<div class="w-full rounded-2xl bg-gray-800 flex flex-col gap-4 p-4 border border-gray-500 mb-4">
+		<div>
+			<h3 class="text-sm font-bold text-gray-400">Client:</h3>
+			<p class="text-lg font-bold">{data.meta.client}</p>
+		</div>
+		<div class="flex gap-8">
+			<div>
+				<h3 class="text-sm font-bold text-gray-400">Date:</h3>
+				<p>{data.meta.year}</p>
+			</div>
+			<div>
+				<h3 class="text-sm font-bold text-gray-400">Duration:</h3>
+				<p>{data.meta.ProjectDuration}</p>
+			</div>
+		</div>
+		<div>
+			<h3 class="text-sm font-bold text-gray-400 mb-2">My Role:</h3>
+			<div class="flex gap-2">
+				{#each data.meta.role as role}
+					<WorkChip label={role} />
+				{/each}
+			</div>
+		</div>
+		<div>
+			<h3 class="text-sm font-bold text-gray-400 mb-2">Stack used:</h3>
+			<div class="flex flex-wrap gap-2">
+				{#each data.meta.stack as stack}
+					<WorkChip label={stack} />
+				{/each}
+			</div>
+		</div>
 	</div>
+
+	{#if data.meta.repository && data.meta.live}
+		<div class="flex gap-4 mb-12 font-bold">
+			<a
+				target="_blank"
+				href={data.meta.repository}
+				class="w-full h-full flex justify-between p-4 py-6 grow rounded-2xl bg-gray-800 border border-gray-600 text-gray-300 hover:border-primary-500 hover:text-primary-500 transition-all duration-200 ease-in-out"
+			>
+				<span> GitHub repository </span>
+				<ExternalLink class="w-6 h-6" />
+			</a>
+
+			<a
+				target="_blank"
+				href={data.meta.live}
+				class="w-full h-full flex justify-between p-4 py-6 grow rounded-2xl bg-gray-800 border border-gray-600 text-gray-300 hover:border-primary-500 hover:text-primary-500 transition-all duration-200 ease-in-out"
+			>
+				<span> live demo </span>
+				<ExternalLink class="w-6 h-6" />
+			</a>
+		</div>
+	{/if}
 
 	<!-- Post -->
 	<div
-		class="mx-auto prose prose-gray dark:prose-invert prose-img:rounded-xl prose-p:text-[#fff4ef] prose-p:text-opacity-75 prose-h2:text-primary-500 prose-headings:mb-2 pb-4"
+		class="mx-auto prose prose-gray dark:prose-invert prose-img:rounded-xl prose-p:text-[#fff4ef] prose-p:text-opacity-75 prose-headings:mb-2 pb-4"
 	>
 		<svelte:component this={data.content} />
 	</div>
