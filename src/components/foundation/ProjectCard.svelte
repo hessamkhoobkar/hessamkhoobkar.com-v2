6a60e5f4e72ff21f5dbb395726a1e1f5025cb0e9
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-01-27 02:42:40 +0330

    Refactor ProjectCard component

--- src/components/foundation/ProjectCard.svelte
+++ src/components/foundation/ProjectCard.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
-	import placeholderBg from '@/assets/images/theme-placeholder-bg.png';
-	export let title: string = 'Coming soon';
-	export let price: string = 'Free';
-	export let image: string = placeholderBg;
+	import WorkChip from '../work/WorkChip.svelte';
+
+	export let title: string = '';
+	export let href: string = '#';
+	export let thumbnail: string = '';
+	export let thumbnailAlt: string = '';
+	export let stack: string[] = [];
 </script>
 
 <div class="w-full rounded-2xl bg-gray-800 border border-gray-600">
-	<a class="w-full h-full flex flex-col p-4" href="/theme/1">
-		<div class="w-full aspect-w-4 aspect-h-3 rounded-2xl overflow-hidden">
-			<img class="object-cover" src={image} alt={title} />
+	<a class="w-full h-full flex flex-col p-4 {href === '#' && 'cursor-default'}" {href}>
+		<div class="w-full aspect-w-4 aspect-h-3 rounded-xl overflow-hidden">
+			<img class="object-cover" src={thumbnail} alt={thumbnailAlt} />
 		</div>
-		<div class="flex justify-between fo font-medium pt-4">
-			<h3>{title}</h3>
-			<span>{price}</span>
+		<div class="flex flex-col justify-between gap-4 font-medium pt-4">
+			<h3 class="text-xl font-bold">{title}</h3>
+			<div class="flex flex-wrap gap-2">
+				{#each stack as label}
+					<WorkChip {label} />
+				{/each}
+			</div>
 		</div>
 	</a>
 </div>
